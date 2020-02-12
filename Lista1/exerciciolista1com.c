bf060acc0b775d2266ddefbcbc93a3304d73cd86
Izabela <58866682+aizaibela@users.noreply.github.com>
2020-02-12 10:37:39 -0300

    Add files via upload

---
+++ Lista1/exerciciolista1com.c
@@ -0,0 +1,64 @@
+//Exercicio1-------------------------------------------
+
+/* letra a : warning de formato errado.
+   letra b : warning de formato errado.
+   letra c : warning de formato errado.
+      */
+  
+  //Exercicio2-------------------------------------------
+#include <stdio.h>
+
+int main(){
+int temperatura = 30;
+int f = (9*temperatura)/5 + 32;
+printf("%d\n", f);
+
+//Exercicio3-------------------------------------------
+int raio = 5;
+float pi = 3.14159;
+float a = (raio*raio)*pi;
+printf("%f\n", a);
+
+}
+ //Exercicio4-------------------------------------------
+
+ #include <stdio.h>
+
+
+int main(){
+int total_vendas = 50 ;
+float comis = (0.15*total_vendas);
+int salario = 1048 + comis;
+
+printf("%f\n", comis);
+printf("%d\n", salario);
+}
+
+//Exercicio5-------------------------------------------
+ #include <stdio.h>
+
+int main(){
+int tempo = 2 ;
+int km = 100;
+float v = km / tempo;
+float litros = 12 * km;
+
+printf("%f\n", v);
+printf("%f\n", litros);
+}
+
+//Exercicio6-------------------------------------------
+ #include <stdio.h>
+
+int main(){
+int dia = 8 ;
+int mes = 4;
+int fdia = 20;
+int fmes = 4;
+int qdias = fdia - dia;
+
+printf("%d\n", qdias);
+}
+
+//Exercicio7-------------------------------------------
+//?
